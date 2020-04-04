28212539221e5fd8a37255eeb56e64aff6d62e35
Sebastian Josef Kerres <sebastian.kerres@rwth-aachen.de>
2020-04-04 13:25:09 +0200

    refactor: restructure homography estimation

--- homography_estimation.py
+++ homography_estimation.py
@@ -63,12 +63,34 @@ def compute_angle(x, y):
     phi = np.arccos(np.dot(x,y) / (np.linalg.norm(x) * np.linalg.norm(y)))
     return phi * 180 / np.pi
 
-def compute_grid_object_frame():
-    grid_object_frame = np.zeros((7,7,2,2))
+  
+def compute_loss(grid_image_frame, intersections, x_dir, y_dir):
+    loss = 0
+    # if np.abs(compute_angle(x_dir, y_dir) - 90) > MAX_GRID_ANGLE_DIFF:
+    #     return LOSS_INV
+    for intersection in intersections:
+        min_dist = np.min(np.linalg.norm(intersection - grid_image_frame, axis = 2))
+        loss = loss + min_dist
+    return loss
+
+def draw_grid(grid_image_frame):
+
     for i in range(7):
-        for j in range(7):
-            grid_object_frame[j][i] = ((i - 3, i - 3), (j - 3, j - 3))
-    return grid_object_frame
+        u_top, v_top = grid_image_frame[0][i] 
+        u_bot, v_bot = grid_image_frame[6][i]
+        plt.plot([u_bot, u_top], [v_bot, v_top], color = 'red')
+
+    for i in range(7):
+        u_left, v_left = grid_image_frame[i][0] 
+        u_right, v_right = grid_image_frame[i][6] 
+        plt.plot([u_left, u_right], [v_left, v_right], color = 'red')
+
+
+def plot_trajectory(trajectory):
+    plt.figure(figsize=[6,8])
+    plt.scatter(trajectory[0], trajectory[1])
+    plt.xlim([-1, 1])
+    plt.ylim([-1, 1])
 
 def compute_grid_image_frame(uv, K, grid_object_frame):
     xy = (uv - K[0:2,2])/np.array([K[0,0], K[1,1]])
@@ -96,7 +118,6 @@ def compute_grid_image_frame(uv, K, grid_object_frame):
 
     x_dir = ((xy[1,:] - xy[0,:]) + (xy[3,:] - xy[2,:])) / 2
     y_dir = ((xy[2,:] - xy[0,:]) + (xy[3,:] - xy[1,:])) / 2
-
     
     # compute the coordinates of the grid intersection lines in camera frame
     grid_camera_frame = grid_object_frame * np.array((x_dir, y_dir))
@@ -105,17 +126,9 @@ def compute_grid_image_frame(uv, K, grid_object_frame):
     # compute the coordinates of the grid intersection lines in image frame (pixel coordinates)
     grid_image_frame = grid_camera_frame * np.array([K[0,0], K[1,1]]) + uv[0,:]
     return True, grid_image_frame, x_dir, y_dir, xy
-  
-def compute_loss(grid_image_frame, intersections, x_dir, y_dir):
-    loss = 0
-    # if np.abs(compute_angle(x_dir, y_dir) - 90) > MAX_GRID_ANGLE_DIFF:
-    #     return LOSS_INV
-    for intersection in intersections:
-        min_dist = np.min(np.linalg.norm(intersection - grid_image_frame, axis = 2))
-        loss = loss + min_dist
-    return loss
 
 def ransac(intersections, K):
+    i = 0
     grid_object_frame = compute_grid_object_frame()
     result = []
     for _ in range(10000):
@@ -126,13 +139,21 @@ def ransac(intersections, K):
         valid, grid_image_frame, x_dir, y_dir, _ = compute_grid_image_frame(uv, K, grid_object_frame)
         if not valid:
             continue
+        i = i +1
         loss = compute_loss(grid_image_frame, intersections, x_dir, y_dir)
         result.append((loss, rand_ind))
     result = np.array(result)
+    print(i)
     # print(result)
     min_los_ind = np.argmin(result[:,0])
     return result[min_los_ind, 1:5]
 
+def compute_grid_object_frame():
+    grid_object_frame = np.zeros((7,7,2,2))
+    for i in range(7):
+        for j in range(7):
+            grid_object_frame[j][i] = ((i - 3, i - 3), (j - 3, j - 3))
+    return grid_object_frame
 
 def estimate_camera_position(intersections, K):
     XY = np.array([(0,0),(1,0),(0,1),(1,1)])
@@ -147,6 +168,7 @@ def estimate_camera_position(intersections, K):
     min_los_ind = ransac(intersections, K)
     uv = np.array(intersections[min_los_ind[0], :])
     grid_object_frame = compute_grid_object_frame()
+    # valid, grid_image_frame, _, _, xy, T = compute_grid_image_frame(uv, K, grid_object_frame, prev_position)
     valid, grid_image_frame, _, _, xy = compute_grid_image_frame(uv, K, grid_object_frame)
     if valid:
         H = estimate_H(xy, XY)
@@ -157,25 +179,9 @@ def estimate_camera_position(intersections, K):
         print("trajectory[0] " + str(position[0]))
         print("trajectory[1] " + str(position[1]))
         print("trajectory[2] " + str(T[2][3]))
+        print("yaw / psi: " + str(-np.arctan2(T[0][2], T[1][2])))
+        print("pitch / theta: " + str(np.arccos(T[2][2])))
+        print("roll / phi: " + str(-np.arctan2(T[0][2], T[1][2])))
         draw_grid(grid_image_frame)
         return True, position
     return False, 0
-
-def draw_grid(grid_image_frame):
-
-    for i in range(7):
-        u_top, v_top = grid_image_frame[0][i] 
-        u_bot, v_bot = grid_image_frame[6][i]
-        plt.plot([u_bot, u_top], [v_bot, v_top], color = 'red')
-
-    for i in range(7):
-        u_left, v_left = grid_image_frame[i][0] 
-        u_right, v_right = grid_image_frame[i][6] 
-        plt.plot([u_left, u_right], [v_left, v_right], color = 'red')
-
-
-def plot_trajectory(trajectory):
-    plt.figure(figsize=[6,8])
-    plt.scatter(trajectory[0], trajectory[1])
-    plt.xlim([-1, 1])
-    plt.ylim([-1, 1])
