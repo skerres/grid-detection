41c60bcb99a6ee5d1b3590737f124dbf478244ea
Sebastian Josef Kerres <sebastian.kerres@rwth-aachen.de>
2020-04-09 12:36:59 +0200

    feature: add function to draw grid on fisheye image

--- fisheye.py
+++ fisheye.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pickle 
 
 import cv2
+from homography_estimation import draw_grid
 
 def camera_to_fisheye(X, Y, Z, intrinsics):
     """
@@ -30,6 +32,17 @@ def camera_to_rectilinear(X, Y, Z, intrinsics):
     v = cy + f * Y/Z
     return [u,v]
 
+def rectilinear_to_fisheye(u_rect, v_rect, K, D):
+    f = K[0,0]
+    cx = K[0,2]
+    cy = K[1,2]
+    k = D[0]
+    phi = np.arctan((v_rect - cy)/(u_rect - cx))
+    theta = np.arctan((u_rect - cx)/(f * np.cos(phi)))
+    u_dst = cx + f * theta * (1 + k * theta**2) * np.cos(phi)
+    v_dst = cy + f * theta * (1 + k * theta**2) * np.sin(phi)
+    return u_dst, v_dst
+
 def undistort_fisheye_image(img_src, K, D):
     """
         returns an undistorted fisheye image
@@ -42,7 +55,8 @@ def undistort_fisheye_image(img_src, K, D):
         D,
         np.eye(3),
         K,
-        (img_src.shape[1], img_src.shape[0]),
+        # (int(img_src.shape[1] * 1.8), int(img_src.shape[0] * 1.1)),
+        (int(img_src.shape[1]), int(img_src.shape[0])),
         cv2.CV_16SC2
     )
 
@@ -54,6 +68,46 @@ def undistort_fisheye_image(img_src, K, D):
         borderMode=cv2.BORDER_CONSTANT
     )
     return img_rect
+
+
+def draw_fisheye_grid(grid_image_frame, grid_color = 'yellow'):
+    def draw_interpolated_line(u_rect_start, v_rect_start, u_rect_finish, v_rect_finish, K, D, points = 4):
+            interpolation = np.zeros((points,2))
+            factor = points / (points - 1)
+            for i in range(interpolation.shape[0]):
+                interpolation[i, 0] = (u_rect_start * (points - i * factor) + u_rect_finish * i * factor) / points
+                interpolation[i, 1] = (v_rect_start * (points - i * factor) + v_rect_finish * i * factor) / points
+
+            for i in range(interpolation.shape[0]):
+                u_dst, v_dst = rectilinear_to_fisheye(interpolation[i, 0], interpolation[i, 1], K, D)
+                interpolation[i, 0] = u_dst
+                interpolation[i, 1] = v_dst
+            print(interpolation)
+            print()     
+            plt.plot(interpolation[:, 0], interpolation[:, 1], color = grid_color)
+
+
+    """
+        grid_image_frame: numpy array (7x7x2)
+
+        draw a grid on the currently active plot using the values on the corners of the grid_image_frame
+    """
+    K, D = load_camera_parameters()
+
+    for i in range(7):
+        for j in range(6):
+            u_rect_top, v_rect_top = grid_image_frame[j][i] 
+            u_rect_bot, v_rect_bot = grid_image_frame[j+1][i]
+            draw_interpolated_line(u_rect_top, v_rect_top, u_rect_bot, v_rect_bot, K, D)
+
+    for i in range(7):
+        for j in range(6):
+            u_rect_left, v_rect_left = grid_image_frame[i][j] 
+            u_rect_right, v_rect_right = grid_image_frame[i][j+1]
+            draw_interpolated_line(u_rect_left, v_rect_left, u_rect_right, v_rect_right, K, D)
+            # u_dst_left, v_dst_left = rectilinear_to_fisheye(u_rect_left, v_rect_left, K, D)
+            # u_dst_right, v_dst_right = rectilinear_to_fisheye(u_rect_right, v_rect_right, K, D)
+            # plt.plot([u_dst_right, u_dst_left], [v_dst_right, v_dst_left], color = grid_color)
 
 
 def load_camera_parameters():
@@ -136,6 +190,26 @@ def convert_fished_folder(input_directory_path, output_directory_path):
             cv2.imwrite(output_directory_path + "/" + file, img_rect)
 
 
-if __name__ == "__main__":
-    convert_fished_folder('/home/sebastian/Studium/Robotic_Vision_TTK_4255/project/grid-detection/data/seq3', '/home/sebastian/Studium/Robotic_Vision_TTK_4255/project/grid-detection/data/defished_seq3')
+def draw_fisheye_grid_image(filepath, grid_image_frame):
+
+    img = plt.imread(filepath)
+    plt.imshow(img)
+    # picklepath = 'data/img0100_grid_image_frame.pickle'
+    # with open(picklepath, 'rb') as handle:
+    #     grid_image_frame_rect = pickle.load(handle)
+    # grid_image_frame_dst = compute_distorted_grid_from_undistorted(grid_image_frame_rect)
+    # print(grid_image_frame_dst)
+    # draw_grid(grid_image_frame_rect, grid_color='red')
+    draw_fisheye_grid(grid_image_frame, grid_color='yellow')
+    plt.xlim([0, img.shape[1]])
+    plt.ylim([img.shape[0], 0])
+
+if __name__ == '__main__':
+    picklepath = 'data/img0100_grid_image_frame.pickle'
+    filepath = 'data/seq1/img0100.jpg'
+    with open(picklepath, 'rb') as handle:
+        grid_image_frame = pickle.load(handle)
+
+    draw_fisheye_grid_image(grid_image_frame, filepath)
+    # convert_fished_folder('/home/sebastian/Studium/Robotic_Vision_TTK_4255/project/grid-detection/data/seq1', '/home/sebastian/Studium/Robotic_Vision_TTK_4255/project/grid-detection/data/defished_seq1_2')
 # test_image_fisheye_distortion()
